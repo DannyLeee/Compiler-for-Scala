c36929652aa5fe8aa0ed7d617e6b633a22278d45
DannyLeee <dannylee94049@gmail.com>
2020-06-01 17:29:32 +0800

    update symbol table
    add type and union value

--- symbolTable.cpp
+++ symbolTable.cpp
@@ -1,30 +1,59 @@
 #include "symbolTable.h"
 
-
-table create() {
-	table sTable;
-	return sTable;
+entry::entry(const dataType& t, const union V& v) : dType(t) {
+	switch (t)
+	{
+	case INT_:
+		val.iVal = v.iVal;
+		break;
+	case REAL_:
+		val.rVal = v.rVal;
+		break;
+	case CHAR_:
+		val.cVal = v.cVal;
+		break;
+	case STARING_:
+		val.sVal = v.sVal;
+		break;
+	case BOOLEAN_:
+		val.bVal = v.bVal;
+		break;
+	default:
+		val.iVal = 0;
+		break;
+	}
 }
 
-int table::lookup(const string &str) {
+entry& entry::operator=(entry& e)
+{
+	dType = e.dType;
+	val.iVal = e.val.iVal;
+	val.rVal = e.val.rVal;
+	val.cVal = e.val.cVal;
+	val.sVal = e.val.sVal;
+	val.bVal = e.val.bVal;
+
+	return *this;
+}
+
+int table::lookup(const string& str) {
 	if (this->entry_.find(str) != this->entry_.end())
 		return 1;
 	return -1;
 }
 
-int table::insert(const string &str, const int &val) {
+int table::insert(const string& str, const dataType& dType, const union entry::V& val) {
 	if (this->lookup(str) == -1) {
-        entry temp;
-		temp.value = val;
+		entry temp(dType, val);
 		this->entry_[str] = temp;
-        return 1;
-    }
-    return -1;
+		return 1;
+	}
+	return -1;
 }
 
 void table::dump() {
 	for (auto it = this->entry_.cbegin(); it != this->entry_.end(); it++) {
 		cout << it->first << endl;
 	}
-	return ;
+	return;
 }
--- symbolTable.h
+++ symbolTable.h
@@ -7,15 +7,36 @@
 using namespace std;
 
 /* symbol table entry */
+enum dataType { INT_, REAL_, CHAR_, STARING_, BOOLEAN_ };
+
 struct entry {
-	int value;
+	enum dataType dType;
+	
+	union V
+	{
+		int iVal;
+		double rVal;
+		char cVal;
+		string* sVal;
+		bool bVal;
+		V() {}
+		V(int i) { iVal = i; }
+		V(double d) { rVal = d; }
+		V(char c) { cVal = c; }
+		V(string* s) { sVal = s; }
+		V(bool b) { bVal = b; }
+		~V() {}
+	}val;
+	
+	entry() {}
+	entry(const dataType& t,const union V& v);
+	entry& operator= (entry& e);
 };
+
 
 struct table {
-    map <string, entry> entry_;
-    int lookup(const string &str);
-    int insert(const string &str, const int &val);
-    void dump();
+	map <string, entry> entry_;
+	int lookup(const string& str);
+	int insert(const string& str, const dataType& dType, const union entry::V& val);
+	void dump();
 };
-
-table create();
